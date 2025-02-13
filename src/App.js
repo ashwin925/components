19c12d0043ff10d7d4e336056ea7dc75250dce55
ashwin925 <ashwinsundar90250@gmail.com>
2025-02-13 06:37:34 +0530

    upt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import IceRevealCard from "./IceRevealCard";
+import IceRevealCard from "./components/ice-reveal-card.tsx";
 
 function App() {
   return (
