1fa329b1ffca3d3e35e818b46e94be2f8e446f92
ashwin925 <ashwinsundar90250@gmail.com>
2025-02-13 06:23:24 +0530

    upt

---
+++ src/App.js
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import IceRevealCard from "./IceRevealCard";
+
+function App() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", background: "#1e1e1e" }}>
+      <IceRevealCard />
+    </div>
+  );
+}
+
+export default App;
