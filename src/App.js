a25dfee4ea06f90e7169355562d328e8ebd91857
ashwin925 <ashwinsundar90250@gmail.com>
2025-02-18 06:39:03 +0530

    new

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from "react";
 import GlassEffect from './components/GlassEffect.tsx';
 import CardReveal from "./components/CharacterReveal.jsx";
+import GlassPanel from './components/glassDiv.jsx';
 
 function App() {
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", background: "#1e1e1e" }}>
       {/* <GlassEffect /> */}
-      <CardReveal />
+      {/* <CardReveal /> */}
+      <GlassPanel />
     </div>
   );
 }
---
+++ src/components/glassDiv.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./glassDiv.css"; 
+
+const GlassPanel = () => {
+  return (
+    <div className="glass-panel">
+      <h1>hi</h1>
+    </div>
+  );
+};
+
+export default GlassPanel;
