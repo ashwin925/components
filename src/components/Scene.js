f132f425a54d9e133f13d0d52da1f7ac4086e4a6
ashwin925 <ashwinsundar90250@gmail.com>
2025-02-19 08:33:19 +0530

    upt

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -2,6 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import { useState } from "react";
 import Glass from "./Glass";
 import ShatteringGlass from "./ShatteringGlass";
+import Content from "./Content";
 
 export default function Scene() {
   const [shattered, setShattered] = useState(false);
@@ -12,16 +13,21 @@ export default function Scene() {
       <Canvas>
         <ambientLight intensity={0.5} />
         <directionalLight position={[2, 2, 2]} />
-
-        {/* Background Content */}
+        
+        {/* Underlying Content */}
         <mesh position={[0, 0, -1]}>
           <planeGeometry args={[2, 2]} />
-          <meshStandardMaterial color={"#ffcc00"} opacity={reveal ? 1 : 0} transparent />
+          <meshStandardMaterial color="#ffcc00" opacity={reveal ? 1 : 0} transparent />
         </mesh>
 
-        {/* Glass Effect */}
-        {!shattered ? <Glass onShatter={() => setShattered(true)} /> : <ShatteringGlass onComplete={() => setReveal(true)} />}
+        {/* Glass or Shattering Effect */}
+        {!shattered ? (
+          <Glass onShatter={() => setShattered(true)} />
+        ) : (
+          <ShatteringGlass onComplete={() => setReveal(true)} />
+        )}
       </Canvas>
+      {reveal && <Content />}
     </div>
   );
 }
