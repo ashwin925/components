edd1afb256e7fce0589d54345fd7cbc99fdc7422
ashwin925 <ashwinsundar90250@gmail.com>
2025-02-20 15:27:46 +0530

    upt

--- src/components/header/SurroundingHeader3D.js
+++ src/components/header/SurroundingHeader3D.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import { Torus, OrbitControls, SpotLight, EffectComposer, Bloom } from "@react-three/drei";
+import { Torus, OrbitControls, SpotLight } from "@react-three/drei";
+import { EffectComposer, Bloom } from "@react-three/postprocessing"; // ✅ Correct Import
 
 export default function GlassHeader3D() {
   return (
