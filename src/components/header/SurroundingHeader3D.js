68d81aedc8a24274ca9b484664bbc062b5257395
ashwin925 <ashwinsundar90250@gmail.com>
2025-02-21 11:59:12 +0530

    hare krishna

--- src/components/header/SurroundingHeader3D.js
+++ src/components/header/SurroundingHeader3D.js
@@ -1,8 +1,7 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { EffectComposer, Bloom } from "@react-three/postprocessing";
-import * as THREE from "three";
 
 export default function GlassHeader3D() {
   return (
@@ -12,13 +11,13 @@ export default function GlassHeader3D() {
         Wrapped Content
       </div>
 
-      <Canvas camera={{ position: [0, 0, 10], fov: 70 }}>
+      <Canvas camera={{ position: [0, 0, 6], fov: 70 }}>
         {/* Lighting */}
         <ambientLight intensity={1.5} />
         <pointLight position={[5, 5, 10]} intensity={3} />
 
-        {/* Curved Glass Header */}
-        <CurvedGlass position={[0, 2, -1]} scale={[4, 1.2, 1]} />
+        {/* Curved Glass Header using CylinderGeometry */}
+        <CurvedGlass position={[0, 2, -1]} scale={[4, 1.2, 0.5]} />
 
         {/* Bloom Effect for Glow */}
         <EffectComposer>
@@ -33,33 +32,21 @@ export default function GlassHeader3D() {
 
 // 3D Curved Glass Component
 function CurvedGlass({ position, scale }) {
-  // Create geometry with curvature applied
-  const geometry = useMemo(() => {
-    const geo = new THREE.PlaneGeometry(6, 2, 64, 64); // More subdivisions for smoother curvature
-
-    // Apply vertex displacement for curvature
-    const pos = geo.attributes.position;
-    for (let i = 0; i < pos.count; i++) {
-      const x = pos.getX(i);
-      const curveAmount = 1.5; // More pronounced curvature
-      const zCurve = Math.sin((x / 3) * Math.PI) * curveAmount;
-      pos.setZ(i, zCurve);
-    }
-    geo.attributes.position.needsUpdate = true;
-
-    return geo;
-  }, []);
-
   return (
-    <mesh geometry={geometry} position={position} scale={scale}>
+    <mesh position={position} scale={scale}>
+      {/* CylinderGeometry: Creates a naturally curved shape */}
+      <cylinderGeometry args={[4, 4, 1.5, 32, 1, true, Math.PI * 0.25, Math.PI * 1.5]} />
+      
+      {/* Glass-like Material */}
       <meshStandardMaterial
         transparent
-        opacity={0.4}
-        roughness={0.05}
-        metalness={1}
+        opacity={0.5}
+        roughness={0.1}
+        metalness={0.9}
         color="#4fa8ff"
         emissive="#4fa8ff"
-        emissiveIntensity={0.9}
+        emissiveIntensity={0.8}
+        side={2} // Double-sided material
       />
     </mesh>
   );
