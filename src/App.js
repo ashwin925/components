cad522bb645fc885e595f03949b7bf497bb83cd7
ashwin925 <ashwinsundar90250@gmail.com>
2025-02-22 16:07:48 +0530

    hare krishna hare rama

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 "use client";
 import React from "react";
+import { Canvas } from '@react-three/fiber';
 import GlassEffect from './components/GlassEffect.js';
 import CardReveal from "./components/characterReveal/CharacterReveal.js";
 import GlassPanel from './components/divComponent/glassDiv.js';
 import Scene from "./components/Scene.js";
 import HeaderWrapper from "./components/header/headerWrap.js";
+import BarrelHeader from "./components/header/headerWrap.js";
 
 function App() {
   return (
@@ -14,7 +16,10 @@ function App() {
       {/* <CardReveal /> */}
     </div>
           <div>
-            <HeaderWrapper />
+          <Canvas>
+            <ambientLight intensity={0.5} />
+            <BarrelHeader />
+          </Canvas>
           </div>
           {/* <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
             <Scene /> 
