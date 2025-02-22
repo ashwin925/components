6acddcbdf6164b27793227a0209f00d4944fe9e4
ashwin925 <ashwinsundar90250@gmail.com>
2025-02-22 16:06:01 +0530

    hare krishna hare rama

--- src/components/header/headerWrap.js
+++ src/components/header/headerWrap.js
@@ -1,13 +1,47 @@
-import React from "react";
-import './headerWrap.css';
+import * as THREE from 'three';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+import { shaderMaterial } from '@react-three/drei';
 
-const HeaderWrap = () => {
+const BarrelDistortionMaterial = shaderMaterial(
+  {
+    time: 0,
+    resolution: new THREE.Vector2(),
+    transparent: true
+  },
+  `
+  varying vec2 vUv;
+  void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  }
+  `,
+  `
+  varying vec2 vUv;
+  uniform float time;
+  void main() {
+    vec2 uv = vUv * 2.0 - 1.0;
+    float strength = 0.3;
+    uv *= mix(1.0, 1.0 - strength * dot(uv, uv), 0.7);
+    vec4 color = vec4(1.0, 1.0, 1.0, 0.2);
+    gl_FragColor = color;
+  }
+  `
+);
+
+const BarrelHeader = () => {
+  const ref = useRef();
+
+  useFrame(({ clock }) => {
+    ref.current.time = clock.getElapsedTime();
+  });
+
   return (
-    <div className="wrapper">
-      <div className="header">Wrapped Header</div>
-      <div className="content">Your content here</div>
-    </div>
+    <mesh>
+      <planeGeometry args={[4, 1.5, 32, 32]} />
+      <barrelDistortionMaterial ref={ref} />
+    </mesh>
   );
 };
 
-export default HeaderWrap;
+export default BarrelHeader;
