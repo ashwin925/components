b1d118514326aa129e5e8b08c3d258f008d8efa5
ashwin925 <ashwinsundar90250@gmail.com>
2025-02-23 09:48:51 +0530

    upt

--- src/components/divComponent/headerWrap.js
+++ src/components/divComponent/headerWrap.js
@@ -4,7 +4,13 @@ import "./headerWrap.css";
 const BarrelGlass = () => {
   return (
     <div className="header-container">
-      <div className="barrel-glass"></div>
+      <div className="barrel-glass">
+        <nav className="nav-links">
+          <span className="glass-text">Home</span>
+          <span className="glass-text">Skills</span>
+          <span className="glass-text">Contact Me</span>
+        </nav>
+      </div>
       <div className="image-holder">
         <img src="your-image.jpg" alt="Profile" className="profile-image" />
       </div>
